7273bf90c6256d2818e26d4b22397bdf60b46059
David Vasek <david.vasek51@gmail.com>
2021-05-26 15:22:19 -0500

    shopping list exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
 'use strict';
+
+console.log('Script Loaded');
+
+const createList = document.querySelector('#createList'),
+  myList = document.querySelector('#myList'),
+  inputList = document.querySelectorAll('input');
+
+
+
+
+function createListFromInput(list) {
+  const listElement = document.createElement('ul');
+
+  list.forEach(function(input) {
+    const listItem = document.createElement('li');
+    listItem.innerText = input.value;
+    listElement.appendChild(listItem);
+  });
+  return listElement;
+}
+
+
+
+
+
+createList.addEventListener('click', function() {
+  const groceryList = createListFromInput(inputList);
+  myList.append(groceryList);
+});
